6075b543b2c293c751f9b7710555f33f6f748b47
Vincent Roumier <vincent.roumier@gameloft.com>
2018-12-19 14:07:47 -0500

    - project

---
+++ RoadRage/Assets/Scripts/Road/Road.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour {
+
+    public float StartSpeed;
+    float currentSpeed;
+
+    Vector2 appliedOffset;
+
+    public SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        appliedOffset = renderer.material.mainTextureOffset;
+        Scroll(StartSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        appliedOffset.y += Time.deltaTime * currentSpeed;
+        renderer.material.mainTextureOffset = appliedOffset;
+    }
+
+    public void Scroll(float speed)
+    {
+        currentSpeed = speed;
+    }
+}
